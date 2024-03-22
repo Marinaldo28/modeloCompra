f1cce7c70e2e287c2ee260956dfd8efc21781717
Marinaldo Junior <marinaldojunior.uepb@gmail.com>
2024-03-22 10:26:04 -0300

    Projeto criado

---
+++ src/main/java/com/marinaldojunior/modeloCompra/ModeloCompraApplication.java
@@ -0,0 +1,13 @@
+package com.marinaldojunior.modeloCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marinaldojunior/modeloCompra/ModeloCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marinaldojunior.modeloCompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
