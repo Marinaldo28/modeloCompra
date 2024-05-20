08948ce7b3556bce2172a87c88a37622953afdc6
Marinaldo Junior <marinaldojunior.uepb@gmail.com>
2024-05-20 11:12:33 -0300

    Criando operação de instanciação

--- src/main/java/com/marinaldojunior/modeloCompra/ModeloCompraApplication.java
+++ src/main/java/com/marinaldojunior/modeloCompra/ModeloCompraApplication.java
@@ -1,13 +1,33 @@
 package com.marinaldojunior.modeloCompra;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marinaldojunior.modeloCompra.domain.Categoria;
+import com.marinaldojunior.modeloCompra.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModeloCompraApplication {
+public class ModeloCompraApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloCompraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria (null, "Informática");
+		Categoria categoria2 = new Categoria (null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 
 }
